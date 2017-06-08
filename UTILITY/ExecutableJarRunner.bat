413c578cb4ebe6185f0c6fb012571bc82ef5f37b
mishra.techie@gmail.com <mishra.techie@gmail.com>
2017-06-08 07:22:43 +0530

    Updated

--- UTILITY/ExecutableJarRunner.bat
+++ UTILITY/ExecutableJarRunner.bat
@@ -1,4 +1,4 @@
-java -jar SprintBootUtilityExecutableJAR-0.0.1-SNAPSHOT.jar --spring.config.location=application.properties
+java -jar SprintBootExeJar-0.0.1-SNAPSHOT.jar --spring.config.location=application.properties
 
 REM --spring.config.location=application.properties --spring.config.name=application.properties
 pause
